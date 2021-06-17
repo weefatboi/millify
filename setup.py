6991e15c2ea0af86b1cff0c69ed49400de541c8f
Gordon Wall <gordon.wall@vk.ai>
2021-06-17 15:14:28 -0400

    version bump and setup naming

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
-from millify import __version__, __author__
 
 setup(
     name='millify',
     packages=['millify'],
-    version=__version__,
+    version="0.1.2+gwall1",
     description='Convert long numbers into a human-readable format in Python',
-    author=__author__,
+    author="Alexander Zaitsev (azaitsev@gmail.com)",
     author_email='azaitsev@gmail.com',
     url='https://github.com/azaitsev/millify',
     keywords=['numbers', 'decimals', 'formatting', 'human-readable', 'pretty'],
